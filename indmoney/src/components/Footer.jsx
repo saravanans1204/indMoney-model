b13aba101efd09b99e59d1c81f39bccbedfbd602
saravanans1204 <saravanans1204@gmail.com>
2023-03-10 18:50:30 +0530

    sdss

--- indmoney/src/components/Footer.jsx
+++ indmoney/src/components/Footer.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import '../css/footer.css'
-
-
+import white from '../assets/white.png'
+import playlogo from '../assets/old-play-store.svg'
+import applogo from '../assets/old-app-store.svg'
 
 const Footer = () => {
   return (
     <div className='body footer'>
         <div className='bottom'>
             <div className='footer-top'>
-                <div><h1>Footer</h1></div>
-                <div><h1>second</h1></div>
+                <div className='left'>
+                  <img className='logo' src={white} alt="logo"/>
+                  <div className='left-buttons'>
+                    <img src={playlogo} alt='play-button'></img>
+                    <img src={applogo} alt='play-button'></img>
+                  </div>
+                </div>
+                <div className='right'>
+                  <h1>second</h1>
+                </div>
                 
             </div>
             
